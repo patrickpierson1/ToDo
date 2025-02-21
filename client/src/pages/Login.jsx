836eaef004aaf860356c9694d8e89f84a73023d1
Milaeris <mila.eris@gmail.com>
2025-02-20 16:54:32 -0800

    basic signup page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,28 +5,27 @@ import React from "react";
 
 const Login = () => {
     return (
-      <div>
-        <div className="login-container">
-              {/* Left Side - Image */}
-              <img src={LoginImage} alt="Login Illustration" className="login-image" />
+        <div>
+            <div className="login-container">
+                {/* Left Side - Image */}
+                <img src={LoginImage} alt="Login Illustration" className="login-image" />
 
-              {/* Right Side - Login Form */}
-              <div className="login-form">
-                <h1 className="login-header">Log In</h1>
+                {/* Right Side - Login Form */}
+                <div className="login-form">
+                    <h1 className="login-header">Log In</h1>
 
-                <label className="login-label">Username</label>
-                <input type="text" className="login-input" placeholder="username" />
+                    <label className="login-label">Username</label>
+                    <input type="text" className="login-input" placeholder="username" />
 
-                <label className="login-label">Password</label>
-                <input type="text" className="login-input" placeholder="password" />
+                    <label className="login-label">Password</label>
+                    <input type="text" className="login-input" placeholder="password" />
 
-                {/* Submit Button */}
-                <button className="login-button">Log In</button>
-              </div>
-
+                    {/* Submit Button */}
+                    <button className="login-button">Log In</button>
+                </div>
             </div>
-      </div>
+        </div>
     );
-  };
+};
 
-  export default Login;
+export default Login;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,34 @@
+import "./../styles/login.css";
+import SignUpImage from "../assets/signup.png";
+import { useState } from "react";
+import React from "react";
+
 const SignUp = () => {
     return (
-      <div>
-        <h1>Sign Up Page</h1>
-        <p>This is the sign-up page.</p>
-      </div>
+        <div>
+            <div className="login-container">
+                {/* Left Side - Image */}
+                <img src={SignUpImage} alt="Sign Up Illustration" className="login-image" />
+
+                {/* Right Side - Login Form */}
+                <div className="login-form">
+                    <h1 className="login-header">Sign Up</h1>
+
+                    <label className="login-label">Username</label>
+                    <input type="text" className="login-input" placeholder="username" />
+
+                    <label className="login-label">Password</label>
+                    <input type="text" className="login-input" placeholder="password" />
+
+                    <label className="login-label">Confirm Password</label>
+                    <input type="text" className="login-input" placeholder="password" />
+
+                    {/* Submit Button */}
+                    <button className="login-button">Submit</button>
+                </div>
+            </div>
+        </div>
     );
-  };
-  
-  export default SignUp;
-  
+};
+
+export default SignUp;
