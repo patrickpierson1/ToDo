c5b3c3ccaf3ca4d0687e6576cf12b57f68a17421
havenec <havenec@wwu.edu>
2025-03-06 23:47:05 -0800

    sign up and login forms working, need to be connected to server side

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -4,6 +4,16 @@ import { useState } from "react";
 import React from "react";
 
 const SignUp = () => {
+  const[username, setUsername] = useState('');
+  const[password, setPassword] = useState('');
+  const[confirmPass, setConfirmPass] = useState('');
+  const[vis, setVis] = useState('password');
+  
+
+  const handleSubmit = (e) => {
+    console.log(username, password, confirmPass);
+  }
+
     return (
         <div>
             <div className="login-container">
@@ -15,13 +25,17 @@ const SignUp = () => {
                     <h1 className="login-header">Sign Up</h1>
 
                     <label className="login-label">Username</label>
-                    <input type="text" className="login-input" placeholder="username" />
+                    <input type="text" className="login-input" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} />
 
                     <label className="login-label">Password</label>
-                    <input type="text" className="login-input" placeholder="password" />
+                    <input type={vis} className="login-input" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
 
                     <label className="login-label">Confirm Password</label>
-                    <input type="text" className="login-input" placeholder="password" />
+                    <input type={vis} className="login-input" placeholder="password" value={confirmPass} onChange={(e) => setConfirmPass(e.target.value)} />
+
+                    <input type="checkbox" name="show" onChange={(e) => {setVis(vis == "password" ? "text":"password")}}/>
+                    <label> Show Password </label>
+
 
                     {/* Submit Button */}
                     <button className="login-button">Submit</button>
