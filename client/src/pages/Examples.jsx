5d04c5c56ead0a61ce00e8ca1e1e5dd2169ca8a2
Milaeris <mila.eris@gmail.com>
2025-03-15 13:04:38 -0700

    added images to examples page

--- client/src/pages/Examples.jsx
+++ client/src/pages/Examples.jsx
@@ -1,8 +1,10 @@
+import Example1 from "../assets/example1.png";
+import Example2 from "../assets/example2.png";
 const Examples = () => {
     return (
         <div className="examples">
-            Example 1: Notes home - insert image
-            Example 2: Notes page - insert image
+            <img src={Example1} alt="Grocery note" className="example-image" />
+            <img src={Example2} alt="Class note" className="example-image" />
         </div>
     )
 }
